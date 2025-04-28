7367afdd642cad4360f91a787b34f418a3028402
Roy Bao <roy@roybao.com>
2025-04-28 10:04:27 -0400

    remove unused code

--- app/analyze/page.tsx
+++ app/analyze/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { submitUrl } from '@/app/actions';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useEffect, useRef, useState } from 'react';
