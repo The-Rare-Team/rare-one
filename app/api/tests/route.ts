be248cff3626aee499a5eaa798df8fdf3bb37b6e
Rai <raimckenzie@users.noreply.github.com>
2025-04-27 21:43:23 -0400

    started tests CRUD, added shadcn

---
+++ app/api/tests/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// export const GET = async function GET(req: NextRequest, { params }: { params: { orgId: string, classId: string } }) {
+
+//     const orgId = (await params).orgId;
+
+//     return NextResponse.json({ message: "ERROR" }, { status: 500 });
+// };
+
+export const GET = async function GET(req: NextRequest) {
+
+    const data_mock = [
+        {
+            id: "cma0etdlm00000cl1hqqxh0m5",
+            name: "Test 1",
+            url: "https://example.com/test1",
+            description: "This is a test of the main user journey.",
+            status: "Passed",
+            createdAt: new Date().toISOString(),
+        },
+        {
+            id: "cma0etkx200050cl1dekift3n",
+            name: "Test 2",
+            url: "https://example.com/test2",
+            description: "This is another test of a secondary user journey.",
+            status: "Failed",
+            createdAt: new Date().toISOString(),
+        }
+    ];
+
+    return NextResponse.json(data_mock, { status: 200 });
+
+};
+
+export const POST = async function POST(req: NextRequest) {
+
+    const data = await req.json();
+
+    console.log("POST data", data);
+
+    return NextResponse.json({ message: "ERROR" }, { status: 500 });
+
+};
--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import TestsTable from "./TestsTable";
+
 const Page = () => {
 
 
   return (
     <div>
-      <h1>Analyze</h1>
-      <p>Analyze your data here.</p>
+      <h1 className="text-2xl font-medium">Tests</h1>
+      <p>View all your previously ran tests here.</p>
+      <div className="mt-3">
+        <TestsTable />
+      </div>
     </div>
   )
 }
